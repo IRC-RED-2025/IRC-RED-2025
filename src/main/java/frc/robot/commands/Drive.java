339ce257774e666f30b8e3fe063e6e3e3f0a6c94
UncleEnk <81113282+UncleEnk@users.noreply.github.com>
2025-10-22 17:54:14 -0700

    Hotfix 2: Installed-java-syntax-highting edition

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Drivetrain;
 
-public class Drive extends Command{
+public class Drive extends Command {
     private Drivetrain drivetrain;
     private CommandXboxController xbox;
 
@@ -13,24 +13,28 @@ public class Drive extends Command{
         this.xbox = xbox;
         addRequirements(drivetrain);
     }
-    
+
     public void execute() {
-        double reduction = 0.66 //I don't trust my teammates
+        double reduction = 0.66; // I don't trust my teammates
+        double yegg = 90 - 15;
+        double xegg = 0 + 15;
 
         double lBoxY = Math.signum(xbox.getLeftY()) * Math.pow(xbox.getLeftY(), 2) * -1;
         double lBoxX = Math.signum(xbox.getLeftX()) * Math.pow(xbox.getLeftX(), 2) * -1;
         double rBoxY = Math.signum(xbox.getRightY()) * Math.pow(xbox.getRightY(), 2) * -1;
-        double lBoxX = Math.signum(xbox.getRightX()) * Math.pow(xbox.getRightX(), 2) * -1;
+        double rBoxX = Math.signum(xbox.getRightX()) * Math.pow(xbox.getRightX(), 2) * -1;
 
-        //drivetrain.go(lBoxY * reduction, lBoxX * reduction); //Marcello idea: one motor joystick X, one motor joystick Y
-        //drivetrain.go(lBoxY * reduction, rBoxY * reduction); //original one motor/one joystick
+        // drivetrain.go(lBoxY * reduction, lBoxX * reduction); //Marcello idea: one
+        // motor joystick X, one motor joystick Y
+        // drivetrain.go(lBoxY * reduction, rBoxY * reduction); //original one motor/one
+        // joystick
 
-        if ((9 * Math.abs(xbox.getLeftX()) <= Math.abs(xbox.getLeftY()) { //Ethan idea
-            drivetrain.go(lBoxY, lBoxY);
-        } else if (9 * Math.abs(xbox.getLeftY()) <= Math.abs(xbox.getLeftX())) {
-            drivetrain.go(lBoxX, -lBoxX);
-        } else { //My idea
-            drivetrain.go((lBoxY+lBoxX)/2*reduction, (lBoxY-lBoxX)/2*reduction);
+        if (Math.abs(Math.atan2(lBoxY, lBoxX)) >= yegg) { // Ethan idea
+            drivetrain.go(lBoxY * reduction, lBoxY * reduction);
+        } else if (Math.abs(Math.atan2(lBoxY, lBoxX)) <= xegg) {
+            drivetrain.go(lBoxX * reduction, -lBoxX * reduction);
+        } else { // My idea
+            drivetrain.go((lBoxY + lBoxX) / 2, (lBoxY - lBoxX) / 2);
         }
     }
 }
