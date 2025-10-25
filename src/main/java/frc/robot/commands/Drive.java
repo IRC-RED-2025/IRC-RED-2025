3f10a6a8dacc2f81beac4b19a3f14e32cb6375ef
UncleEnk <81113282+UncleEnk@users.noreply.github.com>
2025-10-25 10:27:23 -0700

    Added momentary backspin at end of movement to stop drift. Also projected circular input field on square for increased agility (this was hard).

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -7,6 +7,8 @@ import frc.robot.subsystems.Drivetrain;
 public class Drive extends Command {
     private Drivetrain drivetrain;
     private CommandXboxController xbox;
+    boolean wD = false; // wasDriving
+    double pD = 1; // prevDirection
 
     public Drive(Drivetrain drivetrain, CommandXboxController xbox) {
         this.drivetrain = drivetrain;
@@ -16,25 +18,47 @@ public class Drive extends Command {
 
     public void execute() {
         double reduction = 0.66; // I don't trust my teammates
-        double yegg = 90 - 15;
-        double xegg = 0 + 15;
+        double yegg = Math.toRadians(90 - 15);
+        double xegg = Math.toRadians(0 + 15);
 
         double lBoxY = Math.signum(xbox.getLeftY()) * Math.pow(xbox.getLeftY(), 2) * -1;
         double lBoxX = Math.signum(xbox.getLeftX()) * Math.pow(xbox.getLeftX(), 2) * -1;
         double rBoxY = Math.signum(xbox.getRightY()) * Math.pow(xbox.getRightY(), 2) * -1;
         double rBoxX = Math.signum(xbox.getRightX()) * Math.pow(xbox.getRightX(), 2) * -1;
 
+        // Projecting the circular input map onto a square
+        double distSq = Math.sqrt(Math.pow(Math.min(1 / Math.tan(Math.atan2(lBoxX, lBoxY)), 1), 2)
+                + Math.pow(Math.min(Math.tan(Math.atan2(lBoxX, lBoxY)), 1), 2)); // https://www.desmos.com/calculator/8unnybqhh0
+                                                                                 // messy graph but it somewhat explains
+        double sqLBoxX = lBoxX * distSq;
+        double sqLBoxY = lBoxY * distSq;
+
         // drivetrain.go(lBoxY * reduction, lBoxX * reduction); //Marcello idea: one
         // motor joystick X, one motor joystick Y
         // drivetrain.go(lBoxY * reduction, rBoxY * reduction); //original one motor/one
         // joystick
 
-        if (Math.abs(Math.atan2(lBoxY, lBoxX)) >= yegg) { // Ethan idea
-            drivetrain.go(lBoxY * reduction, lBoxY * reduction);
-        } else if (Math.abs(Math.atan2(lBoxY, lBoxX)) <= xegg) {
-            drivetrain.go(lBoxX * reduction, -lBoxX * reduction);
+        if (wD == true && Math.abs(lBoxY) <= 0.01 && Math.abs(lBoxX) <= 0.01) {
+            drivetrain.go(Math.signum(pD) * 0.5, Math.signum(pD) * 0.5);
+        }
+
+        if (Math.atan2(lBoxY, lBoxX) >= yegg || Math.atan2(lBoxY, -lBoxX) >= yegg
+                || Math.atan2(-lBoxY, lBoxX) >= yegg || Math.atan2(-lBoxY, -lBoxX) >= yegg) { // Ethan idea
+
+            drivetrain.go(sqLBoxY * reduction, sqLBoxY * reduction);
+            pD = sqLBoxY;
+
+        } else if (Math.atan2(lBoxY, lBoxX) >= xegg || Math.atan2(lBoxY, -lBoxX) >= xegg
+                || Math.atan2(-lBoxY, lBoxX) >= xegg || Math.atan2(-lBoxY, -lBoxX) >= xegg) {
+
+            drivetrain.go(sqLBoxX * reduction, -sqLBoxX * reduction);
+
         } else { // My idea
-            drivetrain.go((lBoxY + lBoxX) / 2, (lBoxY - lBoxX) / 2);
+            drivetrain.go((sqLBoxY + sqLBoxY) / 2, (sqLBoxY - sqLBoxX) / 2);
+        }
+
+        if (Math.abs(lBoxX) >= 0.01 || Math.abs(lBoxY) >= 0.01) {
+            wD = true;
         }
     }
 }
